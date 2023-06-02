4a35176f1d529e37a79ed805bedeedc253624d66
Lamfi <37219244+Lamfi@users.noreply.github.com>
2023-06-02 16:57:02 +0300

    root fix

--- setup_sdcard.sh
+++ setup_sdcard.sh
@@ -24,13 +24,13 @@ while true; do
         echo -e "Теперь у тебя будет всё автоматизировано, тебе не надо больше будет вводить комманды для доступа к флешке"
         echo -e "Осталась одна поправочка в боксе и жизнь в шоколаде"
         echo -e "Я так же добавлю для тебя некоторые удобные комманды"
-        sudo echo "sudo mount -o remount,exec /Download" >>~/ubuntu/root/.bashrc
-        sudo echo "alias sd=cd /Download/"»~/ubuntu/root/.bashrc
-        sudo echo "alias debug64=WINEPREFIX=~/.wine box64 wine" »~/ubuntu/root/.bashrc
-        sudo echo "alias debug32=WINEPREFIX=~/.wine box86 wine" »~/ubuntu/root/.bashrc
-        sudo echo "alias sd=cd /Download/"»~/ubuntu/root/.bashrc
-        sudo echo "alias debug64all=WINEDEBUG=-all WINEPREFIX=~/.wine box64 wine" »~/ubuntu/root/.bashrc
-        sudo echo "alias debug32all=WINEDEBUG=-all WINEPREFIX=~/.wine box86 wine" »~/ubuntu/root/.bashrc
+        sudo su echo "sudo mount -o remount,exec /Download" >>~/ubuntu/root/.bashrc
+        sudo su echo "alias sd=cd /Download/"»~/ubuntu/root/.bashrc
+        sudo su  echo "alias debug64=WINEPREFIX=~/.wine box64 wine" »~/ubuntu/root/.bashrc
+        sudo su  echo "alias debug32=WINEPREFIX=~/.wine box86 wine" »~/ubuntu/root/.bashrc
+        sudo su  echo "alias sd=cd /Download/"»~/ubuntu/root/.bashrc
+        sudo su  echo "alias debug64all=WINEDEBUG=-all WINEPREFIX=~/.wine box64 wine" »~/ubuntu/root/.bashrc
+        sudo su  echo "alias debug32all=WINEDEBUG=-all WINEPREFIX=~/.wine box86 wine" »~/ubuntu/root/.bashrc
         rm ~/setup_sdcard
         echo -e "Установка завершена :) запускай start-box-root и радуйся :)"  
         break     
