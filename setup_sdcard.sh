26c874a10bc185db4f09b7411bf8f7eb838def46
Lamfi <37219244+Lamfi@users.noreply.github.com>
2023-06-02 17:21:50 +0300

    Update setup_sdcard.sh

--- setup_sdcard.sh
+++ setup_sdcard.sh
@@ -20,14 +20,14 @@ while true; do
         echo -e "Я создал папку для sdcard чтобы был доступ к флешке её в box64droid находится она по пути ~/ubuntu/download"
         echo -e "Теперь введи тот самый номмер тут: например 0403-0203"
         read number;
-        echo "sudo mount --bind /mnt/$number/Download $HOME/ubuntu/Download/ -o -rw" >> .bashrc
+        echo "sudo mount --bind /mnt/$number/Download $HOME/ubuntu/download/ -o -rw" >> .bashrc
         source ~/.bashrc
         echo -e "Теперь у тебя будет всё автоматизировано, тебе не надо больше будет вводить комманды для доступа к флешке"
         echo -e "Осталась одна поправочка в боксе и жизнь в шоколаде"
         echo -e "Я так же добавлю для тебя некоторые удобные комманды"
         su
-        echo "sudo mount -o remount,exec /Download" >> sudo tee -a ~/ubuntu/root/.bashrc
-        echo "alias sd=cd /Download/" >> sudo tee -a ~/ubuntu/root/.bashrc
+        echo "sudo mount -o remount,exec /download" >> sudo tee -a ~/ubuntu/root/.bashrc
+        echo "alias sd=cd /download/" >> sudo tee -a ~/ubuntu/root/.bashrc
         echo "alias debug64=WINEPREFIX=~/.wine box64 wine" >> sudo tee -a ~/ubuntu/root/.bashrc
         echo "alias debug32=WINEPREFIX=~/.wine box86 wine" >> sudo tee -a ~/ubuntu/root/.bashrc
         echo "alias sd=cd /Download/" >> sudo tee -a ~/ubuntu/root/.bashrc
