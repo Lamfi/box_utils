0e9c9344b4aea3d3afc7c2639bc155965c3fec9d
Lamfi <37219244+Lamfi@users.noreply.github.com>
2023-06-02 18:36:47 +0300

    Update setup_sdcard.sh

--- setup_sdcard.sh
+++ setup_sdcard.sh
@@ -25,15 +25,15 @@ while true; do
         echo -e "Теперь у тебя будет всё автоматизировано, тебе не надо больше будет вводить комманды для доступа к флешке"
         echo -e "Осталась одна поправочка в боксе и жизнь в шоколаде"
         echo -e "Я так же добавлю для тебя некоторые удобные комманды"
-        cd ubuntu/root/
-        sudo sh -c mv .bashrc tmpfile"
+        
+        sudo sh -c "mv $HOME/ubuntu/root/.bashrc $HOME/ubuntu/root/tmpfile"
         sudo sh -c "echo 'sudo mount -o remount,exec /download' >>$HOME/ubuntu/root/.bashrc"
         sudo sh -c "echo 'alias sd=\"cd /download/\"' >>$HOME/ubuntu/root/.bashrc"
         sudo sh -c "echo 'alias debug64=\"WINEPREFIX=~/.wine box64 wine\"' >>$HOME/ubuntu/root/.bashrc"
         sudo sh -c "echo 'alias debug32=\"WINEPREFIX=~/.wine box86 wine\"' >>$HOME/ubuntu/root/.bashrc"
         sudo sh -c "echo 'alias debug64all=\"WINEDEBUG=-all WINEPREFIX=~/.wine box64 wine\"' >>$HOME/ubuntu/root/.bashrc"
         sudo sh -c "echo 'alias debug32all=\"WINEDEBUG=-all WINEPREFIX=~/.wine box86 wine\"' >>$HOME/ubuntu/root/.bashrc"
-        sudo sh -c "cat tmpfile >> .bashrc"
+        sudo sh -c "cat $HOME/ubuntu/root/tmpfile >> $HOME/ubuntu/root/.bashrc"
         sudo sh -c "rm tmpfile"
         rm ./setup_sdcard
         echo -e "Установка завершена :) запускай start-box-root и радуйся :)"  
