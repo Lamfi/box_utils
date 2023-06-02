edbd5927d520cd0304545ded4de53081349522db
Lamfi <37219244+Lamfi@users.noreply.github.com>
2023-06-02 17:20:57 +0300

    Update setup_sdcard.sh

--- setup_sdcard.sh
+++ setup_sdcard.sh
@@ -20,7 +20,8 @@ while true; do
         echo -e "Я создал папку для sdcard чтобы был доступ к флешке её в box64droid находится она по пути ~/ubuntu/download"
         echo -e "Теперь введи тот самый номмер тут: например 0403-0203"
         read number;
-        echo "sudo mount --bind /mnt/media_rw/$number/Download $HOME/ubuntu/Download/ -o -rw" >> .bashrc && source ~/.bashrc
+        echo "sudo mount --bind /mnt/$number/Download $HOME/ubuntu/Download/ -o -rw" >> .bashrc
+        source ~/.bashrc
         echo -e "Теперь у тебя будет всё автоматизировано, тебе не надо больше будет вводить комманды для доступа к флешке"
         echo -e "Осталась одна поправочка в боксе и жизнь в шоколаде"
         echo -e "Я так же добавлю для тебя некоторые удобные комманды"
@@ -33,8 +34,10 @@ while true; do
         echo "alias debug64all=WINEDEBUG=-all WINEPREFIX=~/.wine box64 wine" >> sudo tee -a ~/ubuntu/root/.bashrc
         echo "alias debug32all=WINEDEBUG=-all WINEPREFIX=~/.wine box86 wine" >> sudo tee -a ~/ubuntu/root/.bashrc
         exit
+
         rm ~/setup_sdcard
         echo -e "Установка завершена :) запускай start-box-root и радуйся :)"  
+
         break     
         ;;
         *)
